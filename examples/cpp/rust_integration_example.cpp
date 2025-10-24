edceb3ab5f2ff81f21f2e192fbd45c60ce8a5d07
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2025-10-24 11:36:54 +0200

    add working demo with faer still needs c++20 for now

--- examples/cpp/rust_integration_example.cpp
+++ examples/cpp/rust_integration_example.cpp
@@ -1,11 +1,19 @@
-//
-// Copyright (c) 2022 INRIA
-//
+#include <Eigen/Core>
 #include <iostream>
-#include "generated_is_even.h"
-int
-main()
-{
-  std::cout << is_even(2) << std::endl;
-  return 0;
+#include <faer.hpp>
+
+int main() {
+    using T = quad::f128;
+    using Mat = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+
+    Mat A(10, 10);
+    A.setRandom();
+    A = A * A.transpose();
+
+    using namespace faer::linalg::cholesky;
+
+    Mat L = A;
+    llt::factor::in_place(faer::from_eigen(L));
+    L.triangularView<Eigen::StrictlyUpper>().setZero();
+    std::cout << (A - L * L.transpose()).norm() << std::endl;
 }

23f91170ae5a7304e19bc87a3db14f3a2830cb78
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-09-22 09:15:34 +0200

    nix: update

--- flake.nix
+++ flake.nix
@@ -1,81 +1,53 @@
 {
   description = "Advanced Proximal Optimization Toolbox";
 
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
-      { self, lib, ... }:
+    inputs.gepetto.lib.mkFlakoboros inputs (
+      { lib, ... }:
       {
-        systems = inputs.nixpkgs.lib.systems.flakeExposed;
-        flake.overlays = {
-          default = final: prev: {
-            proxsuite = prev.proxsuite.overrideAttrs {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./benchmark
-                  ./bindings
-                  ./cmake-external
-                  ./CMakeLists.txt
-                  ./doc
-                  ./examples
-                  ./include
-                  ./package.xml
-                  ./test
-                ];
-              };
-              postPatch = "";
-            };
-          };
-          eigen_5 = final: prev: {
-            eigen = final.eigen_5;
-            pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-              (python-final: python-prev: {
-                scipy = python-prev.scipy.overrideAttrs {
-                  # broken on linux arm
-                  doInstallCheck = false;
-                };
-              })
+        extraDevPyPackages = [ "proxsuite" ];
+        overrideAttrs.proxsuite = { drv-prev, pkgs-final, ... }: {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./benchmark
+              ./bindings
+              ./cmake
+              ./CMakeLists.txt
+              ./doc
+              ./examples
+              ./include
+              ./package.xml
+              ./test
             ];
           };
+          patches = [ ];
+          cmakeFlags = drv-prev.cmakeFlags ++ [
+            (lib.cmakeBool "BUILD_TESTING" true)
+            (lib.cmakeBool "PROXSUITE_BUILD_EXAMPLES" true)
+            (lib.cmakeBool "BUILD_BENCHMARK" true)
+            (lib.cmakeBool "BUILD_WITH_OPENMP_SUPPORT" true)
+            (lib.cmakeBool "PROXSUITE_BUILD_MAROS_MESZAROS_TESTS" true)
+            (lib.cmakeBool "GENERATE_PYTHON_STUBS" true)
+          ];
+          checkInputs = drv-prev.checkInputs ++ [
+            pkgs-final.catch2_3
+          ];
         };
-        perSystem =
-          {
-            pkgs,
-            pkgs-eigen_5,
-            self',
-            system,
-            ...
-          }:
-          {
-            _module.args = {
-              pkgs = import inputs.nixpkgs {
-                inherit system;
-                overlays = [ self.overlays.default ];
+        extends.eigen5 = final: prev: {
+          eigen = final.eigen_5;
+          pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+            (_python-final: python-prev: {
+              scipy = python-prev.scipy.overrideAttrs {
+                # broken on linux arm
+                doInstallCheck = false;
               };
-              pkgs-eigen_5 = import inputs.nixpkgs {
-                inherit system;
-                overlays = [
-                  self.overlays.eigen_5
-                  self.overlays.default
-                ];
-              };
-            };
-            apps.default = {
-              type = "app";
-              program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
-            };
-            packages = {
-              default = self'.packages.proxsuite;
-              proxsuite = pkgs.python3Packages.proxsuite;
-              proxsuite-eigen_5 = pkgs-eigen_5.python3Packages.proxsuite;
-            };
-          };
+            })
+          ];
+        };
       }
     );
 }
